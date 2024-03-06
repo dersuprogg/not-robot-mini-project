d2fbe2b2f08d555e8effc92a7869a5ed78da0e63
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-06 17:09:48 +0500

    Check user click color against chosen color

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const colorText = document.querySelector(".random-color");
+const colorsContainer = document.querySelector(".colors-container");
 const colorBoxes = document.querySelectorAll(".colors-container__box");
+const continueBtn = document.querySelector(".btn--submit");
 
 const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const letters = ["a", "b", "c", "d", "e", "f"];
@@ -19,10 +21,6 @@ function generateRandColor() {
   return `#${color}`;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  generateColorPalette();
-});
-
 function generateColorPalette() {
   const colorPalette = [];
   for (let i = 0; i < 9; i++) {
@@ -38,3 +36,22 @@ function generateColorPalette() {
   colorText.textContent = selectedColor;
   colorText.style.borderBottom = `4px solid ${selectedColor}`;
 }
+
+function checkUserChosenColor() {
+  colorsContainer.addEventListener("click", (e) => {
+    const selectedColor = colorText.textContent;
+    if (e.target.classList.contains("box-color")) {
+      const userSelectedColor = e.target.textContent;
+      if (userSelectedColor === selectedColor) {
+        continueBtn.classList.remove("btn--disabled");
+      } else {
+        alert("reCaptcha failed");
+        generateColorPalette();
+      }
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", generateColorPalette);
+
+document.addEventListener("DOMContentLoaded", checkUserChosenColor);
